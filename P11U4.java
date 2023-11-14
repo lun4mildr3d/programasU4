07c5fe44830a2d5f2a7c2301c30599ba5cff8ba0
lun4mildr3d <146401129+lun4mildr3d@users.noreply.github.com>
2023-11-13 22:28:10 -0600

    Add files via upload

---
+++ P11U4.java
@@ -0,0 +1,30 @@
+package p11u4;
+
+
+public class P11U4 {
+
+    public static void main(String[] args) {
+        /* int [][] calf = new int [3][3]; //arreglo multidimencional
+        //calf [FILA] [COLUMNA]
+        calf [0][0] = 5;
+        calf [0][1] = 7;
+        calf [0][2] = 9;
+        calf [1][0] = 6;
+        calf [1][1] = 8;
+        calf [1][2] = 7;
+        calf [2][0] = 10;
+        calf [2][1] = 3;
+        calf [2][2] = 1; */
+        int [][] calf = {{5,7,9},{6,8,7},{10,3,1}};
+        System.out.println(calf [0][0]);
+        System.out.println(calf [0][1]);
+        System.out.println(calf [0][2]);
+        System.out.println(calf [1][0]);
+        System.out.println(calf [1][1]);
+        System.out.println(calf [1][2]);
+        System.out.println(calf [2][0]);
+        System.out.println(calf [2][1]);
+        System.out.println(calf [2][2]);
+    }
+    
+}
